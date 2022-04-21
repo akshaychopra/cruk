a7bf3c6ff113542a2ae0dd90aef806ff5b5f9f11
Akshay <akshay@proportunity.co>
2022-04-21 22:38:50 +0100

    add ses email

--- src/util/email.ts
+++ src/util/email.ts
@@ -1,4 +1,38 @@
+import AWS = require('aws-sdk');
+import { String } from 'aws-sdk/clients/appstream';
+import { IDonationsItem } from '../interfaces/donations';
+
+const ses = new AWS.SES();
+
 export const validateEmail = (email:string) => !!email.match(
   // eslint-disable-next-line max-len
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 )?.length;
+
+export const sendThankYouEmail = async (donationItem: IDonationsItem) => {
+  if (!process.env.fromEmail || !donationItem) {
+    // TODO log error;
+    return;
+  }
+  const emailParams = {
+    Destination: {
+      ToAddresses: [donationItem.id],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: `Hello,\n\n We noticed you have made ${donationItem.donations.length} donations to our noble cause. \n\n we'd 
+                    like to thank you for your recent donation`,
+        },
+      },
+      Subject: { Data: 'Your donation is helping others' },
+    },
+    Source: process.env.fromEmail as String,
+  };
+
+  try {
+    await ses.sendEmail(emailParams).promise();
+  } catch (err) {
+    // TODO log silent error
+  }
+};
